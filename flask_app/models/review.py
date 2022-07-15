73eaeb011f72b3a5d749f1f245940aa80374ca7f
dreessen-n <dreessen.edu@gmail.com>
2022-07-15 14:45:09 -0600

    Change get_all to a many to many

--- flask_app/models/review.py
+++ flask_app/models/review.py
@@ -23,6 +23,8 @@ class Review:
         self.user_id = data['user_id']
         # Needed to create this to capture the creator of the review
         self.creator = None
+        self.user_ids_who_favorited = []
+        self.users_who_favorited = []
 
     # CRUD CREATE METHODS
     @classmethod
@@ -31,31 +33,63 @@ class Review:
         query = "INSERT INTO reviews (title, rating, date_watched, content, user_id) VALUES (%(title)s, %(rating)s, %(date_watched)s, %(content)s, %(user_id)s);"
         return connectToMySQL(cls.db).query_db(query,data)
 
-    # CRUD READ METHODS
+    # CRUD READ METHODS -- Modified for many to many
     @classmethod
     def get_all_reviews(cls):
         """Get all the reviews in db"""
-        query = "SELECT * FROM reviews LEFT JOIN users ON reviews.user_id = users.id;"
+        query = '''SELECT * FROM reviews
+                JOIN users AS creators ON reviews.user_id = creators.id
+                LEFT JOIN favorited_reviews ON favorited_reviews.review_id = reviews.id
+                LEFT JOIN users AS users_who_favorited ON favorited_reviews.user_id = users_who_favorited.id;'''
+        # query = "SELECT * FROM reviews LEFT JOIN users ON reviews.user_id = users.id;"
         results = connectToMySQL(cls.db).query_db(query)
         all_reviews = []
         for r in results:
-            # Create the review object
-            review = cls(r)
-            # Create the associated User object; include all contructors
-            user_data = {
-                'id': r['users.id'],
-                'first_name': r['first_name'],
-                'last_name': r['last_name'],
-                'email': r['email'],
-                'password': r['password'],
-                'created_at': r['users.created_at'],
-                'updated_at': r['users.updated_at']
+            new_review = True
+            users_who_favorited_data = {
+                'id': r['users_who_favorited.id'],
+                'first_name': r['users_who_favorited.first_name'],
+                'last_name': r['users_who_favorited.last_name'],
+                'email': r['users_who_favorited.email'],
+                'password': r['users_who_favorited.password'],
+                'created_at': r['users_who_favorited.created_at'],
+                'updated_at': r['users_who_favorited.updated_at']
             }
-            one_user = user.User(user_data)
-            # Set user to creator in review
-            review.creator = one_user
-            # Append the review to the all_review list
-            all_reviews.append(review)
+            # Check to see if previous processed review, exist as current row
+            num_of_review = len(all_reviews)
+            # Check to see if we have reviews in list
+            if num_of_review > 0:
+                # Check if last review equals current row
+                last_review = all_reviews[num_of_review-1]
+                if last_review.id == r['id']:
+                    last_review.user_ids_who_favorited.append(r['users_who_favorited.id'])
+                    last_review.users_who_favorited.append(user.User(users_who_favorited_data))
+                    new_review = False
+            # Create new review object if review has not been created
+            # and added to the list
+            if new_review:
+                # Create the review object
+                review = cls(r)
+                # Create the associated User object; include all contructors
+                user_data = {
+                    'id': r['creators.id'],
+                    'first_name': r['first_name'],
+                    'last_name': r['last_name'],
+                    'email': r['email'],
+                    'password': r['password'],
+                    'created_at': r['creators.created_at'],
+                    'updated_at': r['creators.updated_at']
+                }
+                one_user = user.User(user_data)
+                # Set user to creator in review
+                review.creator = one_user
+                # Check to see if any user liked this review
+                if r['users_who_favorited.id']:
+                    review.user_ids_who_favorited.append(r['users_who_favorited.id'])
+                    review.users_who_favorited.append(user.User(users_who_favorited_data))
+                # Append the review to the all_review list
+                all_reviews.append(review)
+
         return all_reviews
 
     @classmethod
